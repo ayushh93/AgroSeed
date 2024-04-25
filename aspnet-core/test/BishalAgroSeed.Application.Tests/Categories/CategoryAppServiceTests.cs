fa342c98f4e57e37ae2f3191e37b5eef0bb00727
Ayush Karmacharya <ayushkarmacharya220@gmail.com>
2024-04-25 17:37:17 +0545

    feat: Category app service test added

---
+++ aspnet-core/test/BishalAgroSeed.Application.Tests/Categories/CategoryAppServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Azure;
+using BishalAgroSeed.Categories;
+using BishalAgroSeed.Configurations;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace BishalAgroSeed.Application.Tests.Categories
+{
+    public class CategoryAppServiceTests
+    {
+        private readonly CategoryAppService _categoryAppService;
+        private readonly IRepository<Category, Guid> _mockRepository;
+
+        public CategoryAppServiceTests()
+        {
+            _mockRepository = Substitute.For<IRepository<Category, Guid>>();
+            _categoryAppService = new CategoryAppService(_mockRepository);
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldThrowAbpValidationException_WhenCategoryNameIsDuplicate()
+        {
+            // Arrange
+            var exceptionMessage = "Duplicate Category Name!!";
+            var input = new CreateUpdateCategoryDto { DisplayName = "ExistingCategory" };
+
+            //code to return fake response for AnyAsync method: 
+                _mockRepository.AnyAsync(Arg.Any<Expression<Func<Category, bool>>>())
+            .Returns(true);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<AbpValidationException>(() => _categoryAppService.CreateAsync(input));
+
+            // Assert
+            Assert.NotNull(exception);
+            Assert.Contains(exceptionMessage, exception.Message);
+        }
+
+        
+
+    }
+
+}
