185ababb76bcd295fa3b9ed93b99b5651ceaabcd
ayush.karmacharya <ayush.karmacharya@amniltech.com>
2024-05-03 11:25:15 +0545

    feat: Added unit testings for CategoryAppService

--- aspnet-core/test/BishalAgroSeed.Application.Tests/Categories/CategoryAppServiceTests.cs
+++ aspnet-core/test/BishalAgroSeed.Application.Tests/Categories/CategoryAppServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Azure;
 using BishalAgroSeed.Categories;
 using BishalAgroSeed.Configurations;
+using BishalAgroSeed.Dtos;
 using NSubstitute;
 using System;
 using System.Collections.Generic;
@@ -27,14 +28,14 @@ namespace BishalAgroSeed.Application.Tests.Categories
         }
 
         [Fact]
-        public async Task CreateAsync_ShouldThrowAbpValidationException_WhenCategoryNameIsDuplicate()
+        public async Task CreateAsync_ShouldThrowValidationException_WhenCategoryNameIsDuplicate()
         {
             // Arrange
             var exceptionMessage = "Duplicate Category Name!!";
             var input = new CreateUpdateCategoryDto { DisplayName = "ExistingCategory" };
 
-            //code to return fake response for AnyAsync method: 
-                _mockRepository.AnyAsync(Arg.Any<Expression<Func<Category, bool>>>())
+            // Set up the mock repository to return true for the AnyAsync method:
+            _mockRepository.AnyAsync(Arg.Any<Expression<Func<Category, bool>>>())
             .Returns(true);
 
             // Act
@@ -44,8 +45,68 @@ namespace BishalAgroSeed.Application.Tests.Categories
             Assert.NotNull(exception);
             Assert.Contains(exceptionMessage, exception.Message);
         }
+        [Fact]
+        public async Task CreateAsync_ShouldCreateCategory_WhenCategoryNameIsUnique()
+        {
+            // Arrange
+            var input = new CreateUpdateCategoryDto { DisplayName = "NewUniqueCategory", ParentId = null, IsActive = true };
+            var category = new Category(new Guid(), "NewUniqueCategory", new Guid(), true);
 
-        
+            // Set up the mock repository to return false for the AnyAsync method:
+            _mockRepository.AnyAsync(Arg.Any<Expression<Func<Category, bool>>>())
+                .Returns(false);
+
+            // Set up the mock repository for the InsertAsync method:
+            _mockRepository.InsertAsync(Arg.Any<Category>()).Returns(category);
+
+            // Act
+            var result = await _categoryAppService.CreateAsync(input);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.DisplayName);
+            Assert.Equal(input.DisplayName, result.DisplayName);
+        }
+
+        [Fact]
+        public async Task GetCategoriesAsync_ShouldReturnFilteredDropdownDtos()
+        {
+            // Arrange
+            var categories = new List<Category>
+            {
+                new Category(Guid.NewGuid(), "Category1", new Guid(), true),
+                new Category(Guid.NewGuid(), "Category2", new Guid(), false),
+                new Category(Guid.NewGuid(), "Category3", new Guid(), true),
+                new Category(Guid.NewGuid(), "Category4", new Guid(), false),
+                new Category(Guid.NewGuid(), "Category5", new Guid(), true),
+                new Category(Guid.NewGuid(), "Category6", new Guid(), false)
+            };
+
+            var categoryFilter = new GetCategoryFilter { Id = categories[0].Id };
+
+            var expectedResult = new List<DropdownDto> {
+                new DropdownDto(categories[2].Id.ToString(), "Category3"),
+                new DropdownDto(categories[4].Id.ToString(), "Category5"),
+            };
+
+            //Setup mock repository for GetQueryableAsync method to return categories as IQuerable 
+            _mockRepository.GetQueryableAsync().Returns(categories.AsQueryable());
+
+            //Act
+            var result = await _categoryAppService.GetCategoriesAsync(categoryFilter);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult.Count, result.Count);
+            // Compare each property of result and expected result
+            for (int i = 0; i < result.Count; i++)
+            {
+                Assert.Equal(expectedResult[i].Value, result[i].Value);
+                Assert.Equal(expectedResult[i].Name, result[i].Name);
+            };
+            // This failed
+            //Assert.Equal(expectedResult, result); 
+        }
 
     }
 
